1f517b12eb453001f27f5e415cc40185e6898fad
MLxFernando <fernandomacas68@gmail.com>
2025-03-13 06:28:32 -0500

    microservicio-alerta

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Umbrales para alertas
+HIGH_THRESHOLD = 180
+LOW_THRESHOLD = 70
+
+@app.route('/alert', methods=['POST'])
+def alert():
+    data = request.get_json()
+    glucose_level = data['glucose_level']
+    alert_message = ""
+
+    if glucose_level > HIGH_THRESHOLD:
+        alert_message = "¡Alerta! Nivel de glucosa demasiado alto."
+    elif glucose_level < LOW_THRESHOLD:
+        alert_message = "¡Alerta! Nivel de glucosa demasiado bajo."
+
+    if alert_message:
+        return jsonify({"alert": alert_message, "glucose_level": glucose_level}), 200
+    else:
+        return jsonify({"message": "Nivel de glucosa dentro de los límites."}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5002)
